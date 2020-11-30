7fbae8704e2201f3acbf5979de2956bbda0d91ff
theshiveshmishra <ishiveshmishra@gmail.com>
2020-11-30 23:50:45 +0530

    Uploading new portfolio

--- js/custom.js
+++ js/custom.js
@@ -1,61 +1,27 @@
-$( document ).ready(function() {
-            // If a link has a dropdown, add sub menu toggle.
-            $('nav ul li a:not(:only-child)').click(function(e) {
-              $(this).siblings('.nav-dropdown').toggle();
-              // Close one dropdown when selecting another
-              $('.nav-dropdown').not($(this).siblings()).hide();
-              e.stopPropagation();
-            });
-            // Clicking away from dropdown will remove the dropdown class
-            $('html').click(function() {
-              $('.nav-dropdown').hide();
-            });
-            // Toggle open and close nav styles on click
-            $('#nav-toggle').click(function() {
-              $('nav ul').slideToggle();
-            });
-            // Hamburger to X toggle
-            $('#nav-toggle').on('click', function() {
-              this.classList.toggle('active');
-            });
-          });
+$(document).ready(function(){
 
+	// Theme toggle
+	$('.theme-toggle').click(function() {
+		$('body').toggleClass("light");
+		$(this).toggleClass("off");
+		
+		var toggl = $(this);
+		toggl.addClass('scaling');
+		setTimeout(function() {
+		toggl.removeClass('scaling');
+		}, 520);
+	});
 
-$( document ).ready(function() {
-
-  $('.intro-block').fadeIn('slow');
+	// Wrods changer
+	$(function () {
+		count = 0;
+		wordsArray = ["NAMASTE", "MERHABA", "SALAAM", "OLÀ", "BONJOUR", "HOLA", "CIAO"];
+		setInterval(function () {
+			count++;
+			$("#word").fadeOut(400, function () {
+				$(this).text(wordsArray[count % wordsArray.length]).fadeIn(400);
+			});
+		}, 2000);
+	});
 });
-
-$(document).ready(function() {
-    
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-    
-        /* Check the location of each desired element */
-        $('.work-block').each( function(i){
-            
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},1500);
-                    
-            }
-            
-        }); 
-    
-    });
-    
-});
-$(function () {
-  count = 0;
-  wordsArray = ["NAMASTE", "MERHABA", "SALAAM", "OLÀ", "BONJOUR", "HOLA", "CIAO"];
-  setInterval(function () {
-    count++;
-    $("#word").fadeOut(400, function () {
-      $(this).text(wordsArray[count % wordsArray.length]).fadeIn(400);
-    });
-  }, 2000);
-});
+  
