2b57854566d058df149899987348c260a761e443
theshivesh <35421400+theshivesh@users.noreply.github.com>
2023-10-02 13:16:37 +0530

    Pushed fixes for homepage.
    Mobile and desktop polishes and fixes are pushed. Including the navigation

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,3 @@
-// const cricle = document.getElementById("cursor");
-// const circleStyle = cricle.style;
-
-// document.addEventListener("mousemove", (e) => {
-//     circleStyle.top = `${e.clientY - cricle.offsetHeight / 2}px`;
-//     circleStyle.left = `${e.clientX - cricle.offsetWidth / 2}px`;
-// });
-
 $(window).mousemove(function (e) {
     $(".cursor").css({
         left: e.pageX,
@@ -28,34 +20,3 @@ $(".button")
     .on("mouseleave", function () {
         $(".cursor").removeClass("focused-button");
     });
-
-// function setTransform(el, transf) {
-//     el.css("transform", transf);
-//     el.css("-moz-transform", transf);
-//     el.css("-webkit-transform", transf);
-//     el.css("-o-transform", transf);
-//     el.css("-ms-transform", transf);
-// }
-
-// $(function () {
-//     var el = $(".intro"),
-//         mac = $("#mac"),
-//         offset = mac.offset(),
-//         windowHeight = $(window).height();
-
-//     $(window).on("scroll", function () {
-//         $("h1").fadeOut(500);
-
-//         var windowTop = $(window).scrollTop(),
-//             scrollPercent = (offset.top - windowTop) / offset.top,
-//             scale = "scale(" + scrollPercent + ")";
-
-//         setTransform(el, scale);
-
-//         if (windowTop >= 940) {
-//             el.hide();
-//         } else {
-//             el.show();
-//         }
-//     });
-// });
